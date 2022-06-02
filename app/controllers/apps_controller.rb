d0ff0311eaf0f8ddcc4562097e4071d763776134
ahmedezz <ahmedezzcs@gmail.com>
2022-06-02 13:19:07 +0200

    initial commit and initial app controller setup

---
+++ app/controllers/apps_controller.rb
@@ -0,0 +1,39 @@
+require 'digest'
+
+class AppsController < ApplicationController
+  def index
+    @apps = App.all
+    if !@apps.any?
+      return render status: :no_content
+    end
+    return render json: @apps, status: :ok
+  end
+
+  def show
+    @app = App.find_by(token: params[:id])
+    if @app == nil
+      return head status: :not_found
+    end
+    return render json:@app, status: :ok
+  end
+
+  def destroy
+    @app = App.find_by(token: params[:id])
+    if @app == nil
+      return head :not_found
+    end
+
+    return head :no_content
+  end
+
+  def create
+    app = App.new({name: params[:name], token: Digest::MD5.hexdigest(params[:name]) })
+
+    if app.save
+      return render json: app, status: :created
+    else
+      return render json: app.errors, status: :unprocessable_entity
+    end
+  end
+
+end
